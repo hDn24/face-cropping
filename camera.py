af0e3ecb959d469e319d62705ca2823d2fe0457f
Nhan Ho D <nhan.ho@MBA0017.local>
2021-01-18 11:14:04 +0700

    update for camera

--- camera.py
+++ camera.py
@@ -3,7 +3,7 @@ import imutils
 import time
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-ds_factor=0.6
+bgr = [148, 145, 145]
 
 class VideoCamera(object):
     def __init__(self):
@@ -18,26 +18,20 @@ class VideoCamera(object):
 
         success, crop = self.video.read()
 
-        # frame = cv2.resize(crop, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
         frame = imutils.resize(crop, width=600, height=600)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # face_rects = face_cascade.detectMultiScale(gray, 1.1, 5)
         face_rects = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(100, 100))
 
         if len(face_rects):
             for (x, y, w, h) in face_rects:
-                # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 crop = frame[y - 50: y + h + 50, x - 50: x + w + 50]
                 crop = cv2.resize(crop, (200, 250))
-                BLUE = [148, 145, 145]
-                crop = cv2.copyMakeBorder(crop, 100, 100, 150, 150, cv2.BORDER_CONSTANT, value=BLUE)
+                crop = cv2.copyMakeBorder(crop, 100, 100, 150, 150, cv2.BORDER_CONSTANT, value=bgr)
                 break
         else:
-            BLUE = [148, 145, 145]
             crop = cv2.resize(crop, (200, 250))
-            crop = cv2.copyMakeBorder(crop, 100, 100, 150, 150, cv2.BORDER_CONSTANT, value=BLUE)
-            # crop = imutils.resize(crop, width=y, height=x)
-        # cv2.imshow('crop', crop)
+            crop = cv2.copyMakeBorder(crop, 100, 100, 150, 150, cv2.BORDER_CONSTANT, value=bgr)
+
         FPS = round(1 / (time.time() - start))
         cv2.putText(crop, "FPS = {}".format(FPS), (24, 24), cv2.FONT_HERSHEY_SIMPLEX, .5, (0, 24, 248), 2)
 
